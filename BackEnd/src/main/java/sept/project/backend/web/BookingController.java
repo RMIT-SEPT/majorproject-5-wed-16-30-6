29210c1d64a46f6737e996a5d944b00d5d8abaf2
Midori Verdouw <s3575912@student.rmit.edu.au>
2020-08-16 13:25:00 +1000

    Create API and unit test

---
+++ BackEnd/src/main/java/sept/project/backend/web/BookingController.java
@@ -0,0 +1,26 @@
+package sept.project.backend.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sept.project.backend.model.WorkerSchedule;
+import sept.project.backend.services.BookingService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/booking")
+public class BookingController {
+
+    @Autowired
+    private BookingService bookingService;
+
+    @GetMapping(path = "/schedule/{businessId}")
+    public ResponseEntity<?> getBusinessScheduleByBusinessId(@PathVariable("businessId") Long businessId) {
+        Iterable<WorkerSchedule> workerSchedules = bookingService.getAllWorkerScheduleByBusinessId(businessId);
+        return ResponseEntity.ok(workerSchedules);
+    }
+}
