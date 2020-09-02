4effab098b90a5669edc19c5d4f903872394e179
Midori Verdouw <s3575912@student.rmit.edu.au>
2020-09-02 16:22:52 +1000

    Fix test for getBusinessScheduleByBusinessId

--- BackEnd/src/test/java/sept/project/backend/web/BookingControllerTest.java
+++ BackEnd/src/test/java/sept/project/backend/web/BookingControllerTest.java
@@ -1,8 +1,5 @@
 package sept.project.backend.web;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -16,9 +13,13 @@ import sept.project.backend.model.WorkerSchedule;
 import sept.project.backend.services.BookingService;
 
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,25 +44,27 @@ class BookingControllerTest {
     @BeforeEach
     public void setup() throws Exception {
         this.mockMvc = standaloneSetup(this.bookingController).build();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        //sdf.setTimeZone(TimeZone.getTimeZone("UTC+10"));
+        Date startDate1 = sdf.parse("2020-08-17T09:00:00Z");
+        Date endDate1 = sdf.parse("2020-08-17T17:00:00Z");
+        Date startDate2 = sdf.parse("2020-08-18T010:00:00Z");
+        Date endDate2 = sdf.parse("2020-08-18T15:00:00Z");
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd@HH:mm:ss");
-        sdf.setTimeZone(TimeZone.getTimeZone("UTC+10"));
-        Date date1 = sdf.parse("2020-08-17@09:00:00");
-        Date date2 = sdf.parse("2020-08-17@17:00:00");
-
+        // two worker schedules of two different workers of the same business
         WorkerSchedule ws1 = WorkerSchedule.builder()
                 .id(1L)
                 .businessId(1L)
                 .workerId(1L)
-                .startDateTime(date1)
-                .endDateTime(date2)
+                .startDateTime(startDate1)
+                .endDateTime(endDate1)
                 .build();
         WorkerSchedule ws2 = WorkerSchedule.builder()
                 .id(2L)
                 .businessId(1L)
                 .workerId(2L)
-                .startDateTime(date1)
-                .endDateTime(date2)
+                .startDateTime(startDate2)
+                .endDateTime(endDate2)
                 .build();
 
         workerSchedules = new ArrayList<>();
@@ -70,15 +73,26 @@ class BookingControllerTest {
     }
 
     @Test
-    void getBusinessScheduleByBusinessId() throws Exception {
+    void getBusinessScheduleByBusinessIdTest1() throws Exception {
         // Mocking service
         when(bookingService.getAllWorkerSchedulesByBusinessId(any(Long.class))).thenReturn(workerSchedules);
         mockMvc.perform(get("/api/booking/schedule/1").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].businessId", is(1)))
                 .andExpect(jsonPath("$[0].workerId", is(1)))
+                .andExpect(jsonPath("$[0].startDateTime", is("2020-08-17T09:00:00Z")))
+                .andExpect(jsonPath("$[0].endDateTime", is("2020-08-17T17:00:00Z")));
+    }
+
+    @Test
+    void getBusinessScheduleByBusinessIdTest2() throws Exception {
+        // Mocking service
+        when(bookingService.getAllWorkerSchedulesByBusinessId(any(Long.class))).thenReturn(workerSchedules);
+        mockMvc.perform(get("/api/booking/schedule/1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[1].businessId", is(1)))
                 .andExpect(jsonPath("$[1].workerId", is(2)))
-                .andExpect(jsonPath("$[0].startDateTime", is("2020-08-17@09:00:00")))
-                .andExpect(jsonPath("$[0].endDateTime", is("2020-08-17@17:00:00")));
+                .andExpect(jsonPath("$[1].startDateTime", is("2020-08-18T10:00:00Z")))
+                .andExpect(jsonPath("$[1].endDateTime", is("2020-08-18T15:00:00Z")));
     }
 }
