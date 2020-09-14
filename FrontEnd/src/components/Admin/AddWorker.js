8da22a40cf1c7f3119ef3be05697757dd311d63e
Midori Verdouw <s3575912@student.rmit.edu.au>
2020-09-14 15:15:40 +1000

    Add input validation and error messages

--- FrontEnd/src/components/Admin/AddWorker.js
+++ FrontEnd/src/components/Admin/AddWorker.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
-import { addWorker, POST_WORKER_FAILURE } from '../../actions/adminActions';
+import { addWorker } from '../../actions/adminActions';
 import './Admin.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -14,9 +14,19 @@ class AddWorker extends Component {
       desc: "",
       mobile: "",
       startDate: "",
-      endDate: "",
+      endDate: ""
     }
 
+    this.valid = {
+      name: false,
+      personId: false,
+      desc: false,
+      mobile: false,
+      startDate: false,
+      endDate: false
+    }
+
+    this.submit = false;
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -27,44 +37,83 @@ class AddWorker extends Component {
     });
   }
 
-  existEmptyField() {
-    var existingEmptyField = false
+  // set invalid if the input is empty
+  setEmptyFieldInvalid() {
     Object.keys(this.state).forEach(key => {
-      // remove empty space before and after the input string
-      if (!this.state[key].trim()) {
-        existingEmptyField = true;
+      // remove space before & after the input string
+      if (this.state[key].trim()) {
+        this.valid[key] = true;
+      }
+      else {
+        this.valid[key] = false;
+      }
+    });
+  }
+
+  // set valid true if person id is 4 or 5 characters
+  setValidPersonId() {
+    this.valid.personId = (this.state.personId.length >= 4 && this.state.personId.length <= 5);
+  }
+
+  // set valid true if mobile number is 10 characteres
+  setValidMobile() {
+    this.valid.mobile = (this.state.mobile.length === 10)
+  }
+
+  // insert error message in form
+  createErrorMsg(msg, id) {
+    document.getElementById(id).innerHTML = msg;
+  }
+
+  // remove error message
+  removeErrorMsg(id) {
+    document.getElementById(id).innerHTML = "";
+  }
+
+  // validate all fields and set error messages if required
+  validateForm() {
+    this.setEmptyFieldInvalid();
+    this.setValidPersonId();
+    this.setValidMobile();
+
+    const errorMsg = {
+      name: "Name is empty.",
+      personId: "Person ID is invalid.",
+      desc: "Description is empty",
+      mobile: "Mobile number is invalid.",
+      startDate: "Start date is invalid.",
+      endDate: "End date is invalid."
+    }
+
+    // whether all inputs are valid
+    var valid = true;
+    Object.keys(this.valid).forEach(key => {
+      var errorMsgId = key + "-error";
+
+      if (!this.valid[key]) {
+        valid = false;
+        this.createErrorMsg(errorMsg[key], errorMsgId);
+      }
+      else {
+        this.removeErrorMsg(errorMsgId);
       }
     });
 
-    return existingEmptyField;
+    return valid;
   }
 
-  // return true if person id is 4 or 5 characters
-  validatePersonId() {
-    return (this.state.personId.length >= 4 && this.state.personId.length <= 5);
-  }
-
-  // return true if mobile number is 10 characteres
-  validateMobile() {
-    return (this.state.mobile.length == 10);
-  }
-
+  // when submit button is clicked
   handleSubmit(event) {
     event.preventDefault();
+    this.submit = false;
+    
+    // break if any input is invalid
+    if (!this.validateForm()) { return }
 
-    // if empty field exists
-    if (this.existEmptyField()) {
-      document.getElementById('error-msg').innerHTML = "Ensure all fields are completed.";
-      return;
-    }
-
-    // validate fields requirements
-    if (!this.validatePersonId() || !this.validateMobile()) {
-      return;
-    }
-
+    // send state data to backend
     this.props.dispatch(addWorker(this.state));
 
+    this.submit = true;
     this.setState({
       name: "",
       personId: "",
@@ -76,6 +125,7 @@ class AddWorker extends Component {
   }
 
   // called after render()
+  // if the state shifted to submission success, redirect to edit worker page
   componentDidUpdate() {
     if (this.props.addWorkerSuccess) {
       this.props.history.push("/admin/editworker")  
@@ -84,13 +134,23 @@ class AddWorker extends Component {
 
   render() {
     const {name, personId, desc, mobile, startDate, endDate} = this.state;
-    
+    const connectionError = "Error occurred. Please try again.";
+
     return (
       <div id="add-worker">
         <h2>Register Worker</h2>
-        
         <form onSubmit={this.handleSubmit} id="add-worker-form">
-          <div id="error-msg"></div>
+
+          <div id="error-msg">
+            <div>{(this.submit && !this.props.addWorkerSuccess) ? connectionError : ""}</div>
+            <div id="name-error"></div>
+            <div id="personId-error"></div>
+            <div id="mobile-error"></div>
+            <div id="desc-error"></div>
+            <div id="startDate-error"></div>
+            <div id="endDate-error"></div>
+          </div>
+          
           <div className="inputWrapper">
             <label htmlFor="name">Name: </label>
             <div className="input-msg">*Required</div>
@@ -156,7 +216,6 @@ class AddWorker extends Component {
             />
           </div>
 
-          
           <button type="submit" value="Add Worker">Add Worker</button>
         </form>
       </div>
@@ -164,6 +223,11 @@ class AddWorker extends Component {
   }
 };
 
+/**
+ * provide addWorkerSuccess prop to the AddWorker component
+ * which indicates whether submission has succeeded
+ * @param {*} state 
+ */
 const mapStateToProps = state => {
   const currentState = state[state.length - 1]
   return {
