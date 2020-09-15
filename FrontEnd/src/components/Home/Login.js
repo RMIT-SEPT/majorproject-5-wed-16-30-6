fca8841dd235dace8e8a6f467b5362326055514b
Midori Verdouw <s3575912@student.rmit.edu.au>
2020-09-15 15:04:47 +1000

    Add login button for test; minor css change

--- FrontEnd/src/components/Home/Login.js
+++ FrontEnd/src/components/Home/Login.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import LinkButton from './LinkButton';
 
 class Login extends Component {
   render() {
     return (
-      <div>
-        Login Page
+      <div className="login">
+       
+        {/* TODO : This is only for test */}
+        <h1><LinkButton link="/admin/1" label="Click to Login" className="login-test" /></h1>
+        
       </div>
     )
   }
