edaf67f5026dfee105383519b7e40afd794c8dd9
[Julian Tjiong s3786866] <s3786866@student.rmit.edu.au>
2020-09-18 01:07:40 +1000

    Fixed validation type for workerID and businessID

--- BackEnd/src/main/java/sept/project/backend/model/WorkerSchedule.java
+++ BackEnd/src/main/java/sept/project/backend/model/WorkerSchedule.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import java.util.Date;
 
@@ -22,14 +23,14 @@ public class WorkerSchedule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "Business ID is required")
+    @NotNull(message = "Business ID is required")
     private Long businessId;
 
     @JsonFormat(pattern ="yyyy-MM-dd'T'HH:mm:ssXXX")
     private Date startDateTime;
     @JsonFormat(pattern ="yyyy-MM-dd'T'HH:mm:ssXXX")
     private Date endDateTime;
-    @NotBlank
+    @NotNull(message= "worker ID cannot be blank")
     private Long workerId;
 
     public Long getId() {
