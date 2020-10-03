5b0a22c95f1eda4c1bf1cfa2ff577af026293684
Midori Verdouw <s3575912@student.rmit.edu.au>
2020-10-03 13:46:10 +1000

    Replaced Home

--- FrontEnd/src/components/Home/Home.js
+++ FrontEnd/src/components/Home/Home.js
@@ -2,56 +2,28 @@ import React, { Component } from 'react'
 import BusinessOption from './BusinessOption'
 import "./Home.css";
 
+/**
+ * Displayed as the main part of Home page with links to each business
+ */
 class Home extends Component {
+
+  getBusinessOptions() {
+    const businessesCount = 12;
+    var businessOptions = []
+
+    for (var i = 1; i <= businessesCount; i++) {
+      businessOptions.push(
+        <div key={i}><BusinessOption businessId={i}/></div>
+      )
+    }
+
+    return businessOptions;
+  }
+
   render() {
     return (
-      <div>
-        <table id="business-table">
-          <tr>
-            <td>
-              <BusinessOption businessId="1"/>
-            </td>
-            <td>
-              <BusinessOption />
-            </td>
-            <td>
-              <BusinessOption />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <BusinessOption />
-            </td>
-            <td>
-              <BusinessOption />
-            </td>
-            <td>
-              <BusinessOption />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <BusinessOption />
-            </td>
-            <td>
-              <BusinessOption />
-            </td>
-            <td>
-              <BusinessOption />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <BusinessOption />
-            </td>
-            <td>
-              <BusinessOption />
-            </td>
-            <td>
-              <BusinessOption />
-            </td>
-          </tr>
-        </table>
+      <div className="home">
+        {this.getBusinessOptions()}
       </div>
     )
   }
