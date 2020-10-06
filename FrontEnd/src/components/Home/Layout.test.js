9c168e18bcb2b0407e36fdda229f648beac67a7a
Midori Verdouw <s3575912@student.rmit.edu.au>
2020-10-06 21:59:38 +1100

    Change test name

--- FrontEnd/src/components/Home/Layout.test.js
+++ FrontEnd/src/components/Home/Layout.test.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { Header } from "./Header";
+import { Layout } from "./Layout";
 import { shallow, mount } from "enzyme";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("<Header /> component Unit Test", () => {
+describe("<Layout /> component Unit Test", () => {
 
-  it("should render 1 <Header /> component", () => {
-    const wrapper = shallow(<Header />);
+  it("should render 1 <Layout /> component", () => {
+    const wrapper = shallow(<Layout />);
     expect(wrapper).toHaveLength(1);
   });
 
   it("renders '.right-nav'", () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Layout />);
     expect(wrapper.find('.left-nav').length).toBe(1);
   });
 
   it("renders '.right-nav'", () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Layout />);
     expect(wrapper.find('.right-nav').length).toBe(1);
   });
 
