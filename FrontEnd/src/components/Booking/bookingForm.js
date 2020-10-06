f7e9456f287d44aa52337923f5e63eb840904552
Midori Verdouw <s3575912@student.rmit.edu.au>
2020-10-06 16:05:50 +1100

    Fix booking form UI and state; add action dispatch

--- FrontEnd/src/components/Booking/bookingForm.js
+++ FrontEnd/src/components/Booking/bookingForm.js
@@ -2,120 +2,56 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import "./bookingForm.css"
 import LoginSignupPrompt from './LoginSignupPrompt';
-// import ScheduleCell from "./BusinessSchedule/ScheduleCell";
+import { bookService } from '../../actions/bookingActions';
+import { Redirect } from 'react-router-dom';
 
 class bookingForm extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      fields: {
-        Name: "",
-        emailAddress: "",
-        phoneNumber: ""
-      },
-      errors: {}
-    };
-  }
-
-  // handleValidation() {
-  //   let fields = this.state.fields;
-  //   let errors = {};
-  //   let formIsValid = true;
-
-  //   //Name
-  //   if (!fields["Name"]) {
-  //     formIsValid = false;
-  //     errors["Name"] = "Name is Empty";
-  //   }
-  //   if (typeof fields["Name"] !== "undefined") {
-  //     if (!fields["Name"].match(/^[a-zA-Z]+$/)) {
-  //       formIsValid = false;
-  //       errors["Name"] = "Please Enter Letters Only";
-  //     }
-  //   }
-
-  //   //Email Address
-  //   if (!fields["emailAddress"]) {
-  //     formIsValid = false;
-  //     errors["emailAddress"] = "Email Address is Empty";
-  //   }
-  //   if (typeof fields["emailAddress"] !== "undefined") {
-  //     let lastAtPos = fields["emailAddress"].lastIndexOf('@');
-  //     let lastDotComPos = fields["emailAddress"].lastIndexOf('.com');
-
-  //     if (!(lastAtPos < lastDotComPos && lastAtPos > 0 && fields["emailAddress"].indexOf('@@') === -1 && lastDotComPos > 2 && (fields["emailAddress"].length - lastDotComPos) > 2)) {
-  //       formIsValid = false;
-  //       errors["emailAddress"] = "Email is Invalid";
-  //     }
-  //   }
-
-  //   //Phone Number
-  //   if (!fields["phoneNumber"]) {
-  //     formIsValid = false;
-  //     errors["phoneNumber"] = "Phone Number is Empty";
-  //   }
-  //   if (typeof fields["phoneNumber"] !== "undefined") {
-  //     if (!fields["phoneNumber"].match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)) {
-  //       formIsValid = false;
-  //       errors["phoneNumber"] = "Invalid Phone Number"
-  //     }
-  //   }
-  //   this.setState({
-  //     errors: errors
-  //   });
-  //   return formIsValid;
-  // }
-
-  // handleChange(field, e) {
-  //   let fields = this.state.fields;
-  //   fields[field] = e.target.value;
-  //   this.setState({
-  //     fields
-  //   });
-  // }
 
   handleSubmit(e) {
     e.preventDefault();
 
-    if (this.handleValidation()) {
-      alert("Form submitted!");
-    } else {
-      alert("Invalid Form, Unable to Submit!");
-    }
-  }
-
-  getAppointmentMsg() {
-    return (
-      "This is booking form for business "
-      + this.props.location.state.businessId + " at "
-      + this.props.location.state.startTime + " to "
-      + this.props.location.state.endTime + " on "
-      + this.props.location.state.date + "."
-    );
+    // send data to backend
+    this.props.dispatch(bookService(this.props.customer.id, this.props.location.state.scheduleId));
   }
 
   render() {
-    const login = this.props.login;
+    // const login = this.props.login;
     // console.log(this.props.login);
-    // login = true;
+    var login = true;
+
+    if (this.props.booking) {
+      // if the selected schedule (timeslot) is successfully booked
+      if (this.props.booking.id === this.props.location.state.scheduleId) {
+        // TODO: create this page
+        return <Redirect to="/booking/success" />;
+      }
+    }
 
     return (
       <div className="container">
         <h1>Booking Form</h1>
-        <h5>{this.getAppointmentMsg()}</h5>
 
         {!login && <LoginSignupPrompt />}
 
-        {login &&
-          <form onSubmit={this.handleSubmit.bind(this)}>
-            <input type="submit" value="Book Appointment" />
-            <input
-              type="button"
-              value="Cancel"
-              onClick={() => this.props.history.push("business/schedule/" + this.props.location.state.businessId)}
-            />
-          </form>
+        {login && 
+          <div>
+            <div className="booking-info">
+              <div>Customer Name: {this.props.customer.name}</div>
+              <div>Phone: {this.props.customer.mobileNum}</div>
+              <div>Service: {this.props.location.state.businessId}</div>
+              <div>Date: {this.props.location.state.date}</div>
+              <div>Time: {this.props.location.state.startTime} - {this.props.location.state.endTime}</div>
+              <div>Would you like to confirm the booking?</div>
+            </div>
+            <form onSubmit={this.handleSubmit.bind(this)}>
+              <input type="submit" value="Confirm" />
+              <button
+                onClick={() => this.props.history.push("business/schedule/" + this.props.location.state.businessId)}
+                className="cancel"
+              >Cancel
+            </button>
+            </form>
+          </div>
         }
       </div>
     )
@@ -123,11 +59,15 @@ class bookingForm extends Component {
 }
 
 const mapStateToProps = state => {
-  const currentState = state.customerReducer[state.customerReducer.length - 1];
+  const currentCustState = state.customerReducer[state.customerReducer.length - 1];
+  const currentBookingState = state.bookingReducer[state.bookingReducer.length - 1];
+
+  console.log(currentBookingState);
   
   return {
-    login: currentState.login,
-    customer: currentState.customer,
+    login: currentCustState.login,
+    customer: currentCustState.customer,
+    booking: currentBookingState.booking
   }
 }
 
