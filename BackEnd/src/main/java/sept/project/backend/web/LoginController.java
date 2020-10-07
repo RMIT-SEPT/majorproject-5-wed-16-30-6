49cbfe96812af7e064c6cbd469a8379fd52c5305
[Julian Tjiong s3786866] <s3786866@student.rmit.edu.au>
2020-10-07 20:39:02 +1100

    Login backend + tests

---
+++ BackEnd/src/main/java/sept/project/backend/web/LoginController.java
@@ -0,0 +1,39 @@
+package sept.project.backend.web;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import sept.project.backend.model.Person;
+import sept.project.backend.services.MapValidationErrorService;
+import sept.project.backend.services.Personservice;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/login")
+public class LoginController {
+    @Autowired
+    private Personservice personService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @GetMapping("/{personId}/{password}")
+    public ResponseEntity<?> login(@PathVariable String personId, @PathVariable String password)  {
+
+        Person person = personService.findByPersonIdentifier(personId);
+        String passwordTru = person.getPassword();
+
+        if(passwordTru.equals(password)){
+            return new ResponseEntity<Person>(person, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<String>("Wrong credentials mate", HttpStatus.OK);
+        }
+    }
+}
---
+++ BackEnd/src/test/java/sept/project/backend/web/LoginTest.java
@@ -0,0 +1,59 @@
+package sept.project.backend.web;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Rollback;
+import sept.project.backend.model.Person;
+import sept.project.backend.repositories.PersonRepository;
+import sept.project.backend.services.Personservice;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private Personservice personservice;
+
+    @Autowired
+    private LoginController loginController;
+
+    @Test
+    @Rollback(false)
+    public void testLogin() {
+        Long id1= new Long(1);
+        Person person1 = new Person();
+        person1.setRole("w");
+        person1.setMobileNum("0402821822");
+        person1.setDesc("New Worker");
+        person1.setUsername("AAAA");
+        person1.setPassword("asda");
+        person1.setName("John");
+        person1.setId(id1);
+        person1.setAddress("ababa");
+
+        Person personIn = personRepository.save(person1);
+
+        ResponseEntity<?> testOut = loginController.login("AAAA", "baba");
+
+        assertEquals("Wrong credentials mate", testOut,
+                "Test passed cause password is wrong");
+    }
+
+
+
+}
