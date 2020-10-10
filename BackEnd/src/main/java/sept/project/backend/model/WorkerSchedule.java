b1dc00bc0669770d31903f21e594ec9ffe766812
[Julian Tjiong s3786866] <s3786866@student.rmit.edu.au>
2020-10-10 23:56:37 +1100

    Set repositories and services

--- BackEnd/src/main/java/sept/project/backend/model/WorkerSchedule.java
+++ BackEnd/src/main/java/sept/project/backend/model/WorkerSchedule.java
@@ -5,11 +5,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
 
 
 @Entity
@@ -22,16 +24,72 @@ public class WorkerSchedule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "Business ID is required")
+    @NotNull(message = "Business ID is required")
     private Long businessId;
 
     @JsonFormat(pattern ="yyyy-MM-dd'T'HH:mm:ssXXX")
     private Date startDateTime;
     @JsonFormat(pattern ="yyyy-MM-dd'T'HH:mm:ssXXX")
     private Date endDateTime;
-    @NotBlank
+    @NotNull
     private Long workerId;
-    @NotBlank
+    @NotNull
     private Boolean booked;
     private Long custId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBusinessId() {
+        return businessId;
+    }
+
+    public void setBusinessId(Long businessId) {
+        this.businessId = businessId;
+    }
+
+    public Date getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(Date startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public Date getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(Date endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public Long getWorkerId() {
+        return workerId;
+    }
+
+    public void setWorkerId(Long workerId) {
+        this.workerId = workerId;
+    }
+
+    public Boolean getBooked() {
+        return booked;
+    }
+
+    public void setBooked(Boolean booked) {
+        this.booked = booked;
+    }
+
+    public Long getCustId() {
+        return custId;
+    }
+
+    public void setCustId(Long custId) {
+        this.custId = custId;
+    }
 }
