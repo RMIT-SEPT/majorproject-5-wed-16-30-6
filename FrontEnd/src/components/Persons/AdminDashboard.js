86011a3f9ade7a7b969e3247dd122d16adf6baa6
Midori Verdouw <s3575912@student.rmit.edu.au>
2020-10-10 23:08:38 +1100

    Change admin and customer dashboards to be consistent

--- FrontEnd/src/components/Persons/AdminDashboard.js
+++ FrontEnd/src/components/Persons/AdminDashboard.js
@@ -2,72 +2,29 @@ import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./Dashboard.css"
 import AddWorker from '../Admin/AddWorker';
-import EditWorker from '../Admin/EditWorker';
-
+import AdminDashboardColumn from './AdminDashboardColumn';
+import BookingSummary from '../Admin/BookingSummary';
 
 class AdminDashboard extends Component {
-  constructor() {
-    super();
-    this.state = {
-      selected: "Booking Summary"
-    }
-
-    this.options = [
-      "Register Worker", "Edit Worker", "Add/Edit Shift", "Booking Summary", "New Bookings", "View Availability"
-    ];
-  }
-
-  selectRegisterWorker = () => {
-    this.setState({ selected: this.options[0] });
-  };
-
-  selectEditWorker = () => {
-    this.setState({ selected: this.options[1] });
-  };
-
-  getSelectedComponent() {
-    switch(this.state.selected) {
-      case this.options[0]:
-        return <AddWorker />
-      case this.options[1]:
-        return <EditWorker />
-      case this.options[3]:
-        return <div>{this.options[3]}</div>
-      default:
-        return;
-    }
-  }
 
   render() {
-    // const { params } = this.props.match;
-    // const id = parseInt(params.id);
+    var selected = "booking_summary";
+
+    // if redirected from dashboard option
+    if (this.props.location.state) {
+      selected = this.props.location.state.selected;
+    }
+
+    const { params } = this.props.match;
+    const id = parseInt(params.id);
 
     return (
       <div className="dashboard">
-        <div className="dashboard-column">
-          <div className="opt" onClick={this.selectRegisterWorker} >
-            Register Worker
-          </div>
-          <div className="opt" onClick={this.selectEditWorker} >
-            Edit Worker
-          </div>
-          <div className="opt">
-            Add/Edit Shift
-          </div>
-          <div className="opt">
-            Booking Summary
-          </div>
-          <div className="opt">
-            New Bookings
-          </div>
-          <div className="opt">
-            View Availability
-          </div>
-        </div>
+        <AdminDashboardColumn id={id} />
 
-        <div className="dashboard-contents">
-          {this.getSelectedComponent()}
-        </div>
+        {selected === "booking_summary" && <BookingSummary id={id} />}
+        {selected === "add_worker" && <AddWorker id={id} />}
+
       </div>
     )
   }
