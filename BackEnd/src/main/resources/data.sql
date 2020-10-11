78340a8984ec9f0865e57865d22390f88f215c85
Midori Verdouw <s3575912@student.rmit.edu.au>
2020-10-11 18:27:03 +1100

    Add more test data

--- BackEnd/src/main/resources/data.sql
+++ BackEnd/src/main/resources/data.sql
@@ -1,29 +1,99 @@
 /* worker schedule test data */
 /* id, booked, businessId, custId, endDateTime, startDateTime, workerId */
-INSERT INTO Worker_Schedule VALUES (1, false, 1, null, '2020-08-18 10:00:00', '2020-08-18 09:00:00', 1);
-INSERT INTO Worker_Schedule VALUES (2, false, 1, null, '2020-08-18 11:00:00', '2020-08-18 10:00:00', 1);
-INSERT INTO Worker_Schedule VALUES (3, false, 1, null, '2020-08-18 12:00:00', '2020-08-18 11:00:00', 1);
-INSERT INTO Worker_Schedule VALUES (4, false, 1, null, '2020-08-18 13:00:00', '2020-08-18 12:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (1, false, 1, null, '2020-10-18 10:00:00', '2020-10-18 09:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (2, false, 1, null, '2020-10-18 11:00:00', '2020-10-18 10:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (3, false, 1, null, '2020-10-18 12:00:00', '2020-10-18 11:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (4, false, 1, null, '2020-10-18 13:00:00', '2020-10-18 12:00:00', 1);
 
-INSERT INTO Worker_Schedule VALUES (5, false, 1, null, '2020-08-18 10:00:00', '2020-08-18 09:00:00', 2);
-INSERT INTO Worker_Schedule VALUES (6, false, 1, null, '2020-08-18 11:00:00', '2020-08-18 10:00:00', 2);
-INSERT INTO Worker_Schedule VALUES (7, false, 1, null, '2020-08-18 12:00:00', '2020-08-18 11:00:00', 2);
-INSERT INTO Worker_Schedule VALUES (8, false, 1, null, '2020-08-18 13:00:00', '2020-08-18 12:00:00', 2);
-INSERT INTO Worker_Schedule VALUES (9, false, 1, null, '2020-08-18 14:00:00', '2020-08-18 13:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (5, false, 1, null, '2020-10-18 10:00:00', '2020-10-18 09:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (6, false, 1, null, '2020-10-18 11:00:00', '2020-10-18 10:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (7, false, 1, null, '2020-10-18 12:00:00', '2020-10-18 11:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (8, false, 1, null, '2020-10-18 13:00:00', '2020-10-18 12:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (9, false, 1, null, '2020-10-18 14:00:00', '2020-10-18 13:00:00', 2);
+
+INSERT INTO Worker_Schedule VALUES (10, false, 1, null, '2020-10-22 12:00:00', '2020-10-22 11:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (11, false, 1, null, '2020-10-22 13:00:00', '2020-10-22 12:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (12, false, 1, null, '2020-10-22 14:00:00', '2020-10-22 13:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (13, false, 1, null, '2020-10-22 15:00:00', '2020-10-22 14:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (14, false, 1, null, '2020-10-22 16:00:00', '2020-10-22 15:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (15, false, 1, null, '2020-10-22 17:00:00', '2020-10-22 16:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (16, false, 1, null, '2020-10-22 18:00:00', '2020-10-22 17:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (17, false, 1, null, '2020-10-22 19:00:00', '2020-10-22 18:00:00', 1);
+INSERT INTO Worker_Schedule VALUES (18, false, 1, null, '2020-10-22 20:00:00', '2020-10-22 19:00:00', 1);
+
+INSERT INTO Worker_Schedule VALUES (19, false, 1, null, '2020-10-22 12:00:00', '2020-10-22 11:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (20, false, 1, null, '2020-10-22 13:00:00', '2020-10-22 12:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (21, false, 1, null, '2020-10-22 14:00:00', '2020-10-22 13:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (22, false, 1, null, '2020-10-22 15:00:00', '2020-10-22 14:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (23, false, 1, null, '2020-10-22 16:00:00', '2020-10-22 15:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (24, false, 1, null, '2020-10-22 17:00:00', '2020-10-22 16:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (25, false, 1, null, '2020-10-22 18:00:00', '2020-10-22 17:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (26, false, 1, null, '2020-10-22 19:00:00', '2020-10-22 18:00:00', 2);
+INSERT INTO Worker_Schedule VALUES (27, false, 1, null, '2020-10-22 20:00:00', '2020-10-22 19:00:00', 2);
+
+INSERT INTO Worker_Schedule VALUES (28, false, 1, null, '2020-10-20 18:00:00', '2020-10-20 17:00:00', 3);
+INSERT INTO Worker_Schedule VALUES (29, false, 1, null, '2020-10-20 19:00:00', '2020-10-20 18:00:00', 3);
+INSERT INTO Worker_Schedule VALUES (30, false, 1, null, '2020-10-20 20:00:00', '2020-10-20 19:00:00', 3);
+INSERT INTO Worker_Schedule VALUES (31, false, 1, null, '2020-10-20 21:00:00', '2020-10-20 20:00:00', 3);
+INSERT INTO Worker_Schedule VALUES (32, false, 1, null, '2020-10-20 22:00:00', '2020-10-20 21:00:00', 3);
+
+INSERT INTO Worker_Schedule VALUES (33, false, 1, null, '2020-10-21 10:00:00', '2020-10-21 09:00:00', 6);
+INSERT INTO Worker_Schedule VALUES (34, false, 1, null, '2020-10-21 11:00:00', '2020-10-21 10:00:00', 6);
+INSERT INTO Worker_Schedule VALUES (35, false, 1, null, '2020-10-21 12:00:00', '2020-10-21 11:00:00', 6);
+INSERT INTO Worker_Schedule VALUES (36, false, 1, null, '2020-10-21 13:00:00', '2020-10-21 12:00:00', 6);
+INSERT INTO Worker_Schedule VALUES (37, false, 1, null, '2020-10-21 14:00:00', '2020-10-21 13:00:00', 6);
+INSERT INTO Worker_Schedule VALUES (38, false, 1, null, '2020-10-21 15:00:00', '2020-10-21 14:00:00', 6);
+INSERT INTO Worker_Schedule VALUES (39, false, 1, null, '2020-10-21 16:00:00', '2020-10-21 15:00:00', 6);
+
+INSERT INTO Worker_Schedule VALUES (40, false, 1, null, '2020-10-21 10:00:00', '2020-10-21 09:00:00', 7);
+INSERT INTO Worker_Schedule VALUES (41, false, 1, null, '2020-10-21 11:00:00', '2020-10-21 10:00:00', 7);
+INSERT INTO Worker_Schedule VALUES (42, false, 1, null, '2020-10-21 12:00:00', '2020-10-21 11:00:00', 7);
+INSERT INTO Worker_Schedule VALUES (43, false, 1, null, '2020-10-21 13:00:00', '2020-10-21 12:00:00', 7);
+INSERT INTO Worker_Schedule VALUES (44, false, 1, null, '2020-10-21 14:00:00', '2020-10-21 13:00:00', 7);
+INSERT INTO Worker_Schedule VALUES (45, false, 1, null, '2020-10-21 15:00:00', '2020-10-21 14:00:00', 7);
+INSERT INTO Worker_Schedule VALUES (46, false, 1, null, '2020-10-21 16:00:00', '2020-10-21 15:00:00', 7);
+
+INSERT INTO Worker_Schedule VALUES (47, false, 1, null, '2020-10-21 10:00:00', '2020-10-21 09:00:00', 8);
+INSERT INTO Worker_Schedule VALUES (48, false, 1, null, '2020-10-21 11:00:00', '2020-10-21 10:00:00', 8);
+INSERT INTO Worker_Schedule VALUES (49, false, 1, null, '2020-10-21 12:00:00', '2020-10-21 11:00:00', 8);
+INSERT INTO Worker_Schedule VALUES (50, false, 1, null, '2020-10-21 13:00:00', '2020-10-21 12:00:00', 8);
+INSERT INTO Worker_Schedule VALUES (51, false, 1, null, '2020-10-21 14:00:00', '2020-10-21 13:00:00', 8);
+INSERT INTO Worker_Schedule VALUES (52, false, 1, null, '2020-10-21 15:00:00', '2020-10-21 14:00:00', 8);
+INSERT INTO Worker_Schedule VALUES (53, false, 1, null, '2020-10-21 16:00:00', '2020-10-21 15:00:00', 8);
+
+INSERT INTO Worker_Schedule VALUES (54, false, 1, null, '2020-10-21 10:00:00', '2020-10-21 09:00:00', 9);
+INSERT INTO Worker_Schedule VALUES (55, false, 1, null, '2020-10-21 11:00:00', '2020-10-21 10:00:00', 9);
+INSERT INTO Worker_Schedule VALUES (56, false, 1, null, '2020-10-21 12:00:00', '2020-10-21 11:00:00', 9);
+INSERT INTO Worker_Schedule VALUES (57, false, 1, null, '2020-10-21 13:00:00', '2020-10-21 12:00:00', 9);
+INSERT INTO Worker_Schedule VALUES (58, false, 1, null, '2020-10-21 14:00:00', '2020-10-21 13:00:00', 9);
+INSERT INTO Worker_Schedule VALUES (59, false, 1, null, '2020-10-21 15:00:00', '2020-10-21 14:00:00', 9);
+INSERT INTO Worker_Schedule VALUES (60, false, 1, null, '2020-10-21 16:00:00', '2020-10-21 15:00:00', 9);
+
+
+INSERT INTO Worker_Schedule VALUES (61, false, 2, null, '2020-10-19 11:00:00', '2020-10-19 10:00:00', 5);
+INSERT INTO Worker_Schedule VALUES (62, false, 2, null, '2020-10-19 12:00:00', '2020-10-19 11:00:00', 5);
+INSERT INTO Worker_Schedule VALUES (63, false, 2, null, '2020-10-19 13:00:00', '2020-10-19 12:00:00', 5);
+INSERT INTO Worker_Schedule VALUES (64, false, 2, null, '2020-10-19 14:00:00', '2020-10-19 13:00:00', 5);
+INSERT INTO Worker_Schedule VALUES (65, false, 2, null, '2020-10-19 15:00:00', '2020-10-19 14:00:00', 5);
+
 
 /* id, address, created_at, desc, end_date, mobile_num, name, password, role, start_date, updated_at, username */
 INSERT INTO Person VALUES (1, '449 Punt Road, Cremorne', '2020-01-01 00:00:00', 'admin desc',
- '2021-12-31 00:00:00', '9873727793', 'Bob', '123', 'a', '2020-07-30 00:00:00', null, 'BOB77');
+ '2021-12-31 00:00:00', '9873727793', 'Bob', '123', 'w', '2020-07-30 00:00:00', null, 'BOB77');
 INSERT INTO Person VALUES (2, '124 La Trobe St, Melbourne VIC 3000', '2020-01-01 00:00:00', 'worker desc',
  '2021-12-31 00:00:00', '1112223334', 'James', '123', 'w', '2020-07-30 00:00:00', null, 'JAMES');
-INSERT INTO Person VALUES (3, 'address1', '2020-10-11 14:24:14.053', null, null, '5555555555', 'person3', 'v', 'c', null, null, 'PID3');
-INSERT INTO Person VALUES (4, 'address1', '2020-10-11 14:24:14.053', null, null, '5555555555', 'person4', 'v', 'c', null, null, 'PID4');
+INSERT INTO Person VALUES (3, 'address1', '2020-10-11 14:24:14.053', null, null, '5555555555', 'Chris', 'v', 'w', null, null, 'CHRIS');
+INSERT INTO Person VALUES (4, 'address1', '2020-10-11 14:24:14.053', null, null, '5555555555', 'Jane', 'v', 'w', null, null, 'JANE');
+INSERT INTO Person VALUES (5, 'address1', '2020-10-11 14:24:14.053', null, null, '5555555555', 'Erin', 'v', 'w', null, null, 'ERIN');
+INSERT INTO Person VALUES (6, 'address1', '2020-10-11 14:24:14.053', null, null, '5555555555', 'Alex', 'v', 'w', null, null, 'ALEX');
+INSERT INTO Person VALUES (7, 'address1', '2020-10-11 14:24:14.053', null, null, '5555555555', 'Elizabeth', 'v', 'w', null, null, 'ELIZ');
+INSERT INTO Person VALUES (8, 'address1', '2020-10-11 14:24:14.053', null, null, '5555555555', 'Walter', 'v', 'w', null, null, 'WALT');
+INSERT INTO Person VALUES (9, 'address1', '2020-10-11 14:24:14.053', null, null, '5555555555', 'Rebecca', 'v', 'w', null, null, 'REBEC');
+
+INSERT INTO Person VALUES (10, 'address1', '2020-10-11 14:24:14.053', null, null, '5555555555', 'Thomas', 'v', 'a', null, null, 'THOM');
+INSERT INTO Person VALUES (11, '225–245 Plenty Rd, Bundoora', '2020-01-01 00:00:00', 'customer desc',
+ '2021-12-31 00:00:00', '1112223334', 'Emily', '123', 'c', '2020-07-30 00:00:00', null, 'EMILY');
 
 
-INSERT INTO Worker_Schedule VALUES (13, false, 2, null, '2020-08-19 11:00:00', '2020-08-19 10:00:00', 3);
-INSERT INTO Worker_Schedule VALUES (14, false, 2, null, '2020-08-19 12:00:00', '2020-08-19 11:00:00', 3);
-INSERT INTO Worker_Schedule VALUES (15, false, 2, null, '2020-08-19 11:00:00', '2020-08-19 10:00:00', 4);
-INSERT INTO Worker_Schedule VALUES (16, false, 2, null, '2020-08-18 12:00:00', '2020-08-18 11:00:00', 4);
-INSERT INTO Worker_Schedule VALUES (17, false, 2, null, '2020-08-18 13:00:00', '2020-08-18 12:00:00', 4);
 
 
