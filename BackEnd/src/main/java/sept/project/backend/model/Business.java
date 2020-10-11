fb7c94bcb74afe69ebf10faa40522645e5673aad
[Julian Tjiong s3786866] <s3786866@student.rmit.edu.au>
2020-10-11 13:11:49 +1100

    Business table

---
+++ BackEnd/src/main/java/sept/project/backend/model/Business.java
@@ -0,0 +1,53 @@
+package sept.project.backend.model;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.*;
+import java.util.Date;
+
+import lombok.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Entity;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+public class Business {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Person name is required")
+    private String name;
+    private String desc;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
