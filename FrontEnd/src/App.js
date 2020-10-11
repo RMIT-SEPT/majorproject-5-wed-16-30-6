006c32f5fbd952c7efdf5a1cd8b5bcb463ec2d15
Midori Verdouw <s3575912@student.rmit.edu.au>
2020-10-12 00:15:46 +1100

    Fix duplicate components rendered

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -10,8 +10,6 @@ import SignUp from './components/Home/SignUp';
 import About from './components/Home/About';
 import ContactUs from './components/Home/ContactUs';
 import AdminDashboard from './components/Persons/AdminDashboard';
-import AddWorker from './components/Admin/AddWorker';
-import EditWorker from './components/Admin/EditWorker';
 import Profile from './components/Persons/Profile';
 import WorkerDashboard from './components/Persons/WorkerDashboard';
 import CustomerDashboard from './components/Persons/CustomerDashboard';
@@ -33,8 +31,6 @@ function App() {
         <Route path="/user/:id" component={CustomerDashboard} />
         <Route path="/business/:id/schedule" component={BusinessSchedule} />
         <Route path="/bookingForm" component={bookingForm} />
-        <Route path="/admin/:id/addworker" component={AddWorker} />
-        <Route path="/admin/:id/editworker" component={EditWorker} />
         <Route path="/user/:id/profile" component={Profile} />
       </div>
      </BrowserRouter>
